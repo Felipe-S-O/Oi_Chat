6e14cd353c2f27ceaf208038dfe171dd606d479c
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-08-26 10:52:50 -0300

    feat: add menu na tela principal com função de sair e perfil

--- app/src/main/java/br/com/inottec/oichat/MainActivity.kt
+++ app/src/main/java/br/com/inottec/oichat/MainActivity.kt
@@ -1,7 +1,13 @@
 package br.com.inottec.oichat
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import br.com.inottec.oichat.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -35,5 +41,53 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.apply {
             title = "Oi Chat"
         }
+
+        //adicionar menu
+        addMenuProvider(
+            //MenuProvider
+            object : MenuProvider {
+                override fun onCreateMenu(
+                    menu: Menu,
+                    menuInflater: MenuInflater
+                ) {
+                    //inflar o menu
+                    menuInflater.inflate(R.menu.menu_principal, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    //verificar qual item foi clicado
+                    when (menuItem.itemId) {
+                        //quando o item for clicado
+                        R.id.item_perfil -> {
+                            startActivity(
+                                Intent(applicationContext, PerfilActivity::class.java)
+                            )
+
+                        }
+                        //quando o item for clicado
+                        R.id.item_sair -> {
+                            delogarUsuario()
+                        }
+                    }
+                    return true
+                }
+
+                private fun delogarUsuario() {
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle("Atenção")
+                        .setMessage("Deseja realmente sair?")
+                        .setNegativeButton("Não") { dialog, which -> }
+                        .setPositiveButton("Sim") { dialog, which ->
+                            firebaseAuth.signOut()
+                            startActivity(
+                                Intent(applicationContext, LoginActivity::class.java)
+                            )
+                        }
+                        .create()
+                        .show()
+                }
+
+            }
+        )
     }
 }
