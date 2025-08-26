e386a66e8d06ec28a78a3ebb6b27528e804b1e18
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-08-26 10:23:19 -0300

    feat: add Toolbar na tela principal mainActivity

--- app/src/main/java/br/com/inottec/oichat/MainActivity.kt
+++ app/src/main/java/br/com/inottec/oichat/MainActivity.kt
@@ -1,20 +1,39 @@
 package br.com.inottec.oichat
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.com.inottec.oichat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val firebaseAuth by lazy {
+        //Instanciando o Firebase Auth
+        FirebaseAuth.getInstance()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //Configurando a Toolbar
+        inicializarToolbar()
+    }
+
+    private fun inicializarToolbar() {
+        //Configurando a Toolbar
+        val toolbar = binding.includeMainToolbar.tbPrincipal
+        //Setando a Toolbar como a barra de ação
+        setSupportActionBar(toolbar)
+        //Configurando o titulo da Toolbar
+        supportActionBar?.apply {
+            title = "Oi Chat"
         }
     }
 }
