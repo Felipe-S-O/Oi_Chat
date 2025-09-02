43d8e6494aa7764b6e0c3ee7b9c14a1d82a73957
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-09-02 08:27:36 -0300

    feat: add Layout activity perfil

--- app/src/main/java/br/com/inottec/oichat/PerfilActivity.kt
+++ app/src/main/java/br/com/inottec/oichat/PerfilActivity.kt
@@ -1,20 +1,51 @@
 package br.com.inottec.oichat
 
+
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.com.inottec.oichat.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+//
+//    private var temPermissaoGaleria = false
+//    private var temPermissaoCamera = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //Configurando a Toolbar
+        inicializarToolbar()
+
+        //Solicitar permição de galeria e camera
+//        solicitarPermissoes()
+
+    }
+
+//    private fun solicitarPermissoes() {
+//        //Verifica se usuário já tem permissão
+//        temPermissaoCamera = ContextCompat.checkSelfPermission(
+//            this,
+//            Manifest.permission.CAMERA
+//        ) == PackageManager.PERMISSION_GRANTED
+//
+//    }
+
+    private fun inicializarToolbar() {
+        //Configurando a Toolbar
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        //Setando a Toolbar como a barra de ação
+        setSupportActionBar(toolbar)
+        //Configurando o titulo da Toolbar
+        supportActionBar?.apply {
+            title = "Perfil"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
 }
