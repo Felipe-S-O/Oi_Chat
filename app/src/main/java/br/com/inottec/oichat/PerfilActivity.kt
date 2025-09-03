62545862f2ab816c4111a9604fbfe5fc0b30a39a
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-09-02 21:16:57 -0300

    feat: add permisao da camera e galeria

--- app/src/main/java/br/com/inottec/oichat/PerfilActivity.kt
+++ app/src/main/java/br/com/inottec/oichat/PerfilActivity.kt
@@ -3,7 +3,10 @@ package br.com.inottec.oichat
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
 import br.com.inottec.oichat.databinding.ActivityPerfilBinding
@@ -12,9 +15,9 @@ class PerfilActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityPerfilBinding.inflate(layoutInflater)
     }
-//
-//    private var temPermissaoGaleria = false
-//    private var temPermissaoCamera = false
+
+    private var temPermissaoGaleria = false
+    private var temPermissaoCamera = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +27,44 @@ class PerfilActivity : AppCompatActivity() {
         inicializarToolbar()
 
         //Solicitar permição de galeria e camera
-//        solicitarPermissoes()
+        solicitarPermissoes()
 
     }
+    //Solicitar multiplas permissões
+   private val gerenciadorPermissoes = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissoes ->
+        temPermissaoCamera = permissoes[Manifest.permission.CAMERA] ?: false
+        temPermissaoGaleria = permissoes[Manifest.permission.READ_EXTERNAL_STORAGE] ?: false
+    }
 
-//    private fun solicitarPermissoes() {
-//        //Verifica se usuário já tem permissão
-//        temPermissaoCamera = ContextCompat.checkSelfPermission(
-//            this,
-//            Manifest.permission.CAMERA
-//        ) == PackageManager.PERMISSION_GRANTED
-//
-//    }
+    private fun solicitarPermissoes() {
+        //Verifica se usuário já tem permissão
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //Lista de permissões negadas
+        val listaPermissoesNegadas = mutableListOf<String>()
+        if (!temPermissaoCamera)
+            listaPermissoesNegadas.add(Manifest.permission.CAMERA)
+
+        if (!temPermissaoGaleria)
+            listaPermissoesNegadas.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+        if (listaPermissoesNegadas.isNotEmpty())
+            gerenciadorPermissoes.launch(listaPermissoesNegadas.toTypedArray())
+
+
+    }
 
     private fun inicializarToolbar() {
         //Configurando a Toolbar
