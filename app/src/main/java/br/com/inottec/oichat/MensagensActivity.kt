89453464386c3005b2a8eb288731d0d5c81ce044
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-09-20 12:20:16 -0300

    feat: Salvando mensagens

--- app/src/main/java/br/com/inottec/oichat/MensagensActivity.kt
+++ app/src/main/java/br/com/inottec/oichat/MensagensActivity.kt
@@ -4,12 +4,25 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.inottec.oichat.databinding.ActivityMensagensBinding
+import br.com.inottec.oichat.model.Mensagem
 import br.com.inottec.oichat.model.Usuario
 import br.com.inottec.oichat.utils.Constantes
+import br.com.inottec.oichat.utils.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 
 class MensagensActivity : AppCompatActivity() {
 
+    private val firebaseAuth by lazy {
+        //Instanciando o Firebase Auth
+        FirebaseAuth.getInstance()
+    }
+
+    private val firestore by lazy {
+        //Instanciando o Firebase Store
+        FirebaseFirestore.getInstance()
+    }
     private val binding by lazy {
         ActivityMensagensBinding.inflate(layoutInflater)
     }
@@ -21,6 +34,17 @@ class MensagensActivity : AppCompatActivity() {
 
         recuperarDadosUsuarioDestinatario()
         inicializarToolbar()
+        inicializarEventosDeClique()
+    }
+
+    private fun inicializarEventosDeClique() {
+        binding.fabEnviar.setOnClickListener {
+            //Recuperar a mensagem digitada
+            val mensagem = binding.editMensagem.text.toString()
+            if (mensagem.isNotEmpty()) {
+                salvarMensagem(mensagem)
+            }
+        }
     }
 
     private fun inicializarToolbar() {
@@ -31,7 +55,7 @@ class MensagensActivity : AppCompatActivity() {
         //Configurando o titulo da Toolbar
         supportActionBar?.apply {
             title = ""
-            if (dadosDestinatario != null){
+            if (dadosDestinatario != null) {
                 binding.textNome.text = dadosDestinatario!!.nome
                 Picasso.get()
                     .load(dadosDestinatario!!.foto)
@@ -42,20 +66,57 @@ class MensagensActivity : AppCompatActivity() {
         }
     }
 
+    private fun salvarMensagem(TextoMensagem: String) {
+        val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+        val idUsuarioDestinatario = dadosDestinatario?.id
+
+        if (idUsuarioRemetente != null && idUsuarioDestinatario != null) {
+            val mensagem = Mensagem(
+                idUsuarioRemetente,
+                TextoMensagem
+            )
+
+            //Salvar para o remetente
+            salvarMensagemFirestore(idUsuarioRemetente, idUsuarioDestinatario, mensagem)
+            
+            //Salvar para o destinatário
+            salvarMensagemFirestore(idUsuarioDestinatario, idUsuarioRemetente, mensagem)
+
+            binding.editMensagem.setText("")
+        }
+    }
+
+    private fun salvarMensagemFirestore(
+        idUsuarioRemetente: String,
+        idUsuarioDestinatario: String,
+        mensagem: Mensagem
+    ) {
+        firestore
+            .collection(Constantes.BD_MENSAGENS)
+            .document(idUsuarioRemetente)
+            .collection(idUsuarioDestinatario)
+            .add(mensagem)
+            .addOnFailureListener {
+                exibirMensagem("Erro ao enviar mensagem")
+            }
+    }
+
     private fun recuperarDadosUsuarioDestinatario() {
         val extras = intent.extras
         if (extras != null) {
             val origem = extras.getString("origem")
-            if (origem == Constantes.ORIGEM_CONTATO){
+            if (origem == Constantes.ORIGEM_CONTATO) {
 
                 dadosDestinatario = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                     extras.getParcelable("dadosDestinatario", Usuario::class.java)
-                }else{
+                } else {
                     extras.getParcelable("dadosDestinatario")
                 }
-            }else if (origem == Constantes.ORIGEM_CONVERSAO){
+            } else if (origem == Constantes.ORIGEM_CONVERSAO) {
 
             }
         }
     }
 }
+
+
--- app/src/main/java/br/com/inottec/oichat/utils/Constantes.kt
+++ app/src/main/java/br/com/inottec/oichat/utils/Constantes.kt
@@ -3,4 +3,7 @@ package br.com.inottec.oichat.utils
 object Constantes {
     const val ORIGEM_CONTATO = "origem_contato";
     const val ORIGEM_CONVERSAO = "origem_conversao";
+
+    const val BD_MENSAGENS = "mensagens";
+
 }
